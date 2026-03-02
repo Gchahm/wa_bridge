d4502c6297339bfc78c21522a9021a39db28b6a8
gustavoch <gustavo.francelino@gmail.com>
2026-03-02 10:13:06 -0300

    chore: fix formatting in Vite config and wrangler.jsonc files

--- wa-sales/vite.config.ts
+++ wa-sales/vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import {cloudflare} from "@cloudflare/vite-plugin";
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
